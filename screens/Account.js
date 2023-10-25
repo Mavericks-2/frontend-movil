17c70074ad1cc24229191e3ba34bae1aa18c32b1
Angel Garcia <angel_012912@hotmail.com>
2023-10-24 20:07:37 -0600

    Primera version account page

--- screens/Account.js
+++ screens/Account.js
@@ -1,21 +1,86 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import React from "react";
+import CardInfo from "../components/CardInfo";
+import { LinearGradient } from "expo-linear-gradient";
+import colors from "../constants/colors";
 
-const Account = () => {
+const Account = (props) => {
   return (
     <View style={styles.container}>
-      <Text>Account</Text>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Ejemplo Nombre</Text>
+        <Text style={styles.body}>Ejemplo Correo</Text>
+      </View>
+      <View style={styles.cardsContainer}>
+        <CardInfo />
+        <CardInfo />
+        <CardInfo />
+        <CardInfo />
+        <CardInfo />
+      </View>
+      <Pressable onPress={() => props.navigation.navigate("Login")}>
+        <LinearGradient
+          colors={[colors.PRIMARY, colors.SECONDARY]}
+          start={[0, 0]}
+          end={[1, 1]}
+          location={[0.25, 1]}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Cerrar sesión</Text>
+        </LinearGradient>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff",
-    }
-})
+  container: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 64,
+    backgroundColor: "#fff",
+  },
+  cardsContainer: {
+    width: "80%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    columnGap: 32,
+    rowGap: 40,
+  },
+  headerContainer: {
+    width: "80%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 12,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  body: {
+    fontSize: 16,
+    color: "#71727A",
+  },
+  button: {
+    width: "100%",
+    height: 48,
+    color: "white",
+    borderRadius: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 16,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: "white",
+    textAlign: "center",
+  },
+});
